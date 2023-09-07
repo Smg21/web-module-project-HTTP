05b92caa7a8686ee95f7ec7dd3592ac5c7fc56fe
Sabrina <Smg21022@gmail.com>
2023-09-07 13:39:21 -0700

    srta fixed

--- src/App.js
+++ src/App.js
@@ -1,64 +1,4 @@
-// import React, { useEffect, useState } from "react";
 
-// import { Route, Routes, Navigate } from "react-router-dom";
-// import MovieList from './components/MovieList';
-// import Movie from './components/Movie';
-
-// import MovieHeader from './components/MovieHeader';
-
-// import FavoriteMovieList from './components/FavoriteMovieList';
-
-// import axios from 'axios';
-
-// const App = (props) => {
-//   const [movies, setMovies] = useState([]);
-//   const [favoriteMovies, setFavoriteMovies] = useState([]);
-
-//   useEffect(() => {
-//     axios.get('http://localhost:9000/api/movies')
-//       .then(res => {
-//         setMovies(res.data);
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//   }, []);
-
-//   const deleteMovie = (id) => {
-//   }
-
-//   const addToFavorites = (movie) => {
-
-//   }
-
-//   return (
-//     <div>
-//       <nav className="navbar navbar-dark bg-dark">
-//         <span className="navbar-brand" > HTTP / CRUD Module Project</span>
-//       </nav>
-
-//       <div className="container">
-//         <MovieHeader />
-//         <div className="row ">
-//           <FavoriteMovieList favoriteMovies={favoriteMovies} />
-
-//           <Routes>
-//             <Route path="movies/edit/:id" />
-
-//             <Route path="movies/:id" />
-
-//             <Route path="movies" element={<MovieList movies={movies} />} />
-
-//             <Route path="/" element={<Navigate to="/movies" />} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-
-// export default App;
 import React, { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import MovieList from './components/MovieList';
@@ -82,11 +22,11 @@ const App = (props) => {
   }, []);
 
   const addToFavorites = (movie) => {
-    // Check if the movie is already in favorites
+
     const isAlreadyFavorite = favoriteMovies.some(favMovie => favMovie.id === movie.id);
 
     if (!isAlreadyFavorite) {
-      // Add the movie to the list of favorite movies
+
       setFavoriteMovies(prevFavorites => [...prevFavorites, movie]);
     }
   };
--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,41 +1,16 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
 
-// const MovieListItem = (props) => {
-//   const { id, title, director, genre, metascore } = props.movie;
-//   const toggleFavorite = () => {
-  
-//     addToFavorites(movie);
-//   };
-
-//   return (<tr key={id}>
-//     <td>{title}</td>
-//     <td>{director}</td>
-//     <td>{genre}</td>
-//     <td>{metascore}</td>
-//     <td>
-//       <Link to={`/movies/${id}`} className="view">
-//       <button onClick={toggleFavorite}>Favorite</button>
-//         <input type="button" className="btn btn-secondary" value="View" />
-//       </Link>
-//     </td>
-//   </tr>);
-// }
-
-// export default MovieListItem;
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const MovieListItem = (props) => {
   const { id, title, director, genre, metascore } = props.movie;
 
-  // Define addToFavorites function
   const addToFavorites = () => {
-    // Add the movie to the list of favorites
+
     setFavorites((prevFavorites) => [...prevFavorites, props.movie]);
   };
 
-  // State to keep track of favorite movies
+
   const [favorites, setFavorites] = useState([]);
 
   return (
